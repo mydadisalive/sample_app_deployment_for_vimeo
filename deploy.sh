d008a0d3a0ccec2acb77069297e05b596352012d
mydadisalive <mydadisalive@gmail.com>
2020-03-04 13:52:08 +0000

    fixed some cosmetics

--- deploy.sh
+++ deploy.sh
@@ -15,45 +15,51 @@ APP_ARCHIVE_DIR="/home/app_archives"
 PROJECT_NAME="sample-project"
 LOG_FILE="/var/log/${PROJECT_NAME}/deployments.log"
 
-MACHINES=(devopstask1 devopstask2)  # MACHINES contains the machines to deploy to
-declare -A HOST_TO_INSTANCE_ID      # HOST_TO_INSTANCE_ID maps host to its AWS instance-id
+MACHINES=(devopstask1 devopstask2)  # MACHINES contains the machines to deploy to. Add more here if needed
+declare -A HOST_TO_INSTANCE_ID      # HOST_TO_INSTANCE_ID maps host to its AWS instance-id. Initialized in main
 
 # FUNCTIONS
+
+# initizlize instance ids
 function initializeInstanceIds()
 {
+	echoAndLog "initializing instance ids"
 	for i in "${MACHINES[@]}"; do
 		echoAndLog "initializing instandid for server $i"
 		HOST_TO_INSTANCE_ID["$i"]=$(ssh $i wget -q -O - http://169.254.169.254/latest/meta-data/instance-id)
 	done
 }
 
+# trap ctrc-c and log it
 function ctrlc()
 {
 	echoAndLog "deployment interrupted by ctrl-c"
 	exit 1
 }
 
+# logger function
 function echoAndLog()
 {
 	TEXT_TO_ECHO_AND_LOG=$1
 	
-	echo "${TEXT_TO_ECHO_AND_LOG}"
+	echo "- ${TEXT_TO_ECHO_AND_LOG}"
 	sudo bash -c "echo `date` : ${TEXT_TO_ECHO_AND_LOG} >> ${LOG_FILE}" 
 }
 
+# main deploy function
 function deploy()
 {
 	ENV="${1}"
 
-	echoAndLog "deploying app to ${ENV}"
+	echoAndLog "deploying app with ENV=${ENV}"
 
 	# replace DEV to $ENV in settings.py
 	echoAndLog "injecting $ENV to ${SETTINGS_FILE}"
-	changeKeyToValInFile "CONFIG" "${ENV}" "${SETTINGS_FILE}" "=" | sudo tee -a $LOG_FILE 2>&1
+	setKeyToValInFile "CONFIG" "${ENV}" "${SETTINGS_FILE}" "=" | sudo tee -a $LOG_FILE 2>&1
 
 	# raise version in version file
 	echoAndLog "raising version in ${VERSION_FILE} from ${CURRENT_VERSION} to ${NEXT_VERSION}"
-	changeKeyToValInFile "VERSION" "${NEXT_VERSION}" "${VERSION_FILE}" ":" | sudo tee -a $LOG_FILE 2>&1
+	setKeyToValInFile "VERSION" "${NEXT_VERSION}" "${VERSION_FILE}" ":" | sudo tee -a $LOG_FILE 2>&1
 
 	# create an archive of the version
 	echoAndLog "creating and archive of the current version"
@@ -62,11 +68,13 @@ function deploy()
 	# deploy app main loop, detach from ELB, clean previous installation, and deploy new app
 	echoAndLog "deploying app to deployment servers"
 	initializeInstanceIds
+	
+	# main loop
 	for host in "${MACHINES[@]}"; do
 		instance_id="${HOST_TO_INSTANCE_ID[$host]}"
 		
-		# detach from ELB
-		echoAndLog "detach $host with instance-id ${instance_id} from ELB"
+		# deregister host from ELB
+		echoAndLog "deregistering $host with instance-id ${instance_id} from ELB"
 		aws elb deregister-instances-from-load-balancer --load-balancer-name devopstask-elb --instances $instance_id --output text | sudo tee -a $LOG_FILE
 		sleep 1
 		
@@ -79,7 +87,7 @@ function deploy()
 		scp -Cr "${APP_ARCHIVE_DIR}/${PROJECT_NAME}.${DATE}.${NEXT_VERSION}.tgz" "${host}:/tmp" | sudo tee -a $LOG_FILE 2>&1
 		ssh $host "(cd $APP_HOME ; sudo tar xvf /tmp/${PROJECT_NAME}.${DATE}.${NEXT_VERSION}.tgz ; rm /tmp/${PROJECT_NAME}.${DATE}.${NEXT_VERSION}.tgz)" | sudo tee -a $LOG_FILE 2>&1
 
-		# registering host back to ELB
+		# register host back to ELB
 		echoAndLog "registering $host with instance-id $instance_id back to ELB"
 		aws elb register-instances-with-load-balancer --load-balancer-name devopstask-elb --instances $instance_id --output text | sudo tee -a $LOG_FILE
 		sleep 1
@@ -88,6 +96,7 @@ function deploy()
 
 }
 
+# archive artifacts
 function createArchive()
 {
 	DATE=$(date "+%Y-%m-%d")
@@ -96,7 +105,8 @@ function createArchive()
 	sudo bash -c "(cd ${APP_HOME} ; tar --exclude='.git' -zcvf ${APP_ARCHIVE_DIR}/${PROJECT_NAME}.${DATE}.${NEXT_VERSION}.tgz . ) | sudo tee -a ${LOG_FILE} 2>&1"
 }
 
-function changeKeyToValInFile()
+# helper function that sets key to val in a file based on a delimiter (some files are in key=val and others are in key:val form)
+function setKeyToValInFile()
 {
 	KEY=$1
 	VAL=$2
@@ -104,9 +114,9 @@ function changeKeyToValInFile()
 	DELIMITER=$4
 
 	sudo sed -i "s/^\($KEY[ \t]*$DELIMITER[ \t]*\).*\$/\1$VAL/" "${FILE}" | sudo tee -a $LOG_FILE 2>&1
-
 }
 
+# get the version from the version file
 function getVersion()
 {
 
@@ -114,6 +124,7 @@ function getVersion()
 	echo "${VERSION}"
 }
 
+# return the next version
 function raiseVersion()
 {
 	# raise version
@@ -121,8 +132,8 @@ function raiseVersion()
 
 }
 
-
-function help_menu ()
+# usage
+function usage()
 {
 cat << EOF
 Usage: ${0} (-h | -d ENV)
@@ -136,22 +147,24 @@ EOF
 CURRENT_VERSION=$(getVersion)
 NEXT_VERSION=$(raiseVersion)
 
-echoAndLog "deployment started"
-sleep 2
 # check number of args is valid
-[[ $# == 0 ]] && help_menu && exit 1
+[[ $# == 0 ]] && usage && exit 1
+
+echoAndLog "------------------"
+echoAndLog "deployment started"
+echoAndLog "------------------"
 
 while [[ $# > 0 ]]
 do
 case "${1}" in
 	-d|--deploy)
 	ENV="${2}"
-	[[ "$ENV" != "DEV" && "${ENV}" != "PROD" ]] && help_menu && exit 1
+	[[ "$ENV" != "DEV" && "${ENV}" != "PROD" ]] && usage && exit 1
 	deploy "${2}"
 	shift
 	;;
 	-h|--help)
-	help_menu
+	usage
 	shift
 	;;
 	*)
@@ -161,4 +174,6 @@ esac
 shift
 done
 
+echoAndLog "----------------"
 echoAndLog "deployment ended"
+echoAndLog "----------------"
